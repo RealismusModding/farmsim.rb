d18c51c1dbd3631e56f543ea8e417985ab8e4df3
Jos Kuijpers <jkuijpers@jarvix.nl>
2017-06-06 22:54:04 +0200

    Add more todos

---
+++ lib/farmsim/cli/server/install.rb
@@ -0,0 +1,11 @@
+
+
+
+# http://ruby-doc.org/stdlib-2.4.1/libdoc/net/ftp/rdoc/Net/FTP.html
+
+# stop server (call into stop command)
+# open FTP session
+# remove file, if exists
+# upload binary file
+# close FTP session
+# start server (call into start command)
